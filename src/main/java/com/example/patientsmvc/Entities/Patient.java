f6f21684b806f3a401c2f8d55f150a5912a3af7d
Hamza <hamzakarkouri83@gmail.com>
2023-11-23 20:44:32 +0100

    Second commit

--- src/main/java/com/example/patientsmvc/Entities/Patient.java
+++ src/main/java/com/example/patientsmvc/Entities/Patient.java
@@ -1,23 +1,26 @@
 package com.example.patientsmvc.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class Patient {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length=50)
+    @NotEmpty
+    @Size(min = 4, max = 20)
     private String nom;
     @Temporal(TemporalType.DATE)
     private Date dateNaissance;
     private boolean malade;
+    @Min(10)
     private int score;
 }
--- src/main/java/com/example/patientsmvc/web/PatientController.java
+++ src/main/java/com/example/patientsmvc/web/PatientController.java
@@ -2,11 +2,17 @@ package com.example.patientsmvc.web;
 
 import com.example.patientsmvc.Entities.Patient;
 import com.example.patientsmvc.repositories.PatientRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,11 +21,39 @@ import java.util.List;
 public class PatientController {
     @Autowired
     private PatientRepository patientRepository;
-@GetMapping(path="/index")
-    public String patients(Model model){
-    List<Patient> patients=patientRepository.findAll();
-    model.addAttribute("listPatients",patients);
-return "patients";
+    @GetMapping("/index")
+    public String index(Model model,
+                        @RequestParam(name = "page",defaultValue = "0") int page,
+                        @RequestParam(name = "size",defaultValue = "5") int size,
+                        @RequestParam(name = "keyword",defaultValue = "") String kw
+    ){
+        Page<Patient> pagePatients = patientRepository.findByNomContains(kw, PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",kw);
+        return "patients";
     }
-
+    @GetMapping("/deletePatient")
+    public String deletePatient(@RequestParam(name = "id") Long id, String keyword, int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/formPatient")
+    public String formPatient(Model model ){
+        model.addAttribute("patient",new Patient());
+        return "formPatient";
+    }
+    @PostMapping("/savePatient")
+    public String savePatient(@Valid Patient patient, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) return "formPatient";
+        patientRepository.save(patient);
+        return "formPatient";
+    }
+    @GetMapping("/editPatient")
+    public String editPatient(@RequestParam(name = "id") Long id, Model model){
+        Patient patient=patientRepository.findById(id).get();
+        model.addAttribute("patient",patient);
+        return "editPatient";
+    }
 }
